097e110ac8fa7b2de8f7d77a7230259e8ab1ec17
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2026-02-27 11:33:02 +0100

    les 1 en 2

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<!-- les 1 en 2 -->
+<div class="container-lg">
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-3 me-5 bg-primary text-danger">
+            <p class="w-75 bg-secondary fs-2">Col 1</p>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 bg-danger">Col 2</div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
