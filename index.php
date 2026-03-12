9f81c48f99c82dd6901d12ae62831ad2255a5574
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2026-03-12 09:44:59 +0100

    les 4

--- index.php
+++ index.php
@@ -6,51 +6,113 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
 <!-- les 3 -->
-<nav class="navbar navbar-expand-sm bg-primary" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Bedrijfsnaam</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<!--<nav class="navbar navbar-expand-sm bg-primary" data-bs-theme="dark">-->
+<!--    <div class="container-fluid">-->
+<!--        <a class="navbar-brand" href="index.php">Bedrijfsnaam</a>-->
+<!--        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--            <span class="navbar-toggler-icon"></span>-->
+<!--        </button>-->
+<!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--            <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" aria-current="page" href="index.php">Home</a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="#">Link</a>-->
+<!--                </li>-->
+<!--                <li class="nav-item dropdown">-->
+<!--                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">-->
+<!--                        Dropdown-->
+<!--                    </a>-->
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a class="dropdown-item" href="#">Action</a></li>-->
+<!--                        <li><a class="dropdown-item" href="#">Another action</a></li>-->
+<!--                        <li><hr class="dropdown-divider"></li>-->
+<!--                        <li><a class="dropdown-item" href="#">Something else here</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" aria-disabled="true">Disabled</a>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
 <!-- les 1 en 2 -->
-<div class="container-lg">
+<!--<div class="container-lg">-->
+<!--    <div class="row">-->
+<!--        <div class="col-sm-12 col-md-6 col-lg-3 me-5 bg-primary text-danger">-->
+<!--            <p class="w-75 bg-secondary fs-2">Col 1</p>-->
+<!--        </div>-->
+<!--        <div class="col-sm-12 col-md-6 col-lg-3 bg-danger">Col 2</div>-->
+<!--    </div>-->
+<!--</div>-->
+
+<div class="container-fluid">
     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-3 me-5 bg-primary text-danger">
-            <p class="w-75 bg-secondary fs-2">Col 1</p>
+        <div class="col-sm-12 col-md-6 col-lg-3 bg-primary">
+            text
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-3 bg-danger">Col 2</div>
+        <div class="col-sm-12 col-md-6 col-lg-3 bg-secondary">
+            text
+        </div>
+        <div class="col-sm-12 col-md-7 col-lg-3 bg-success">
+            text
+        </div>
+        <div class="col-sm-12 col-md-5 col-lg-3 bg-danger">
+            <img src="img/logo.jpeg" alt="logo" class="img-fluid rounded">
+        </div>
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<!-- les 4 -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card-group pb-2">
+                <div class="card">
+                    <img src="img/logo.png" class="card-img-top" alt="logo">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+                            consectetur dicta dolor dolore dolores error fugit hic illo inventore labore quas reiciendis
+                            voluptas, voluptate? A aliquid amet aperiam aut autem, culpa deserunt eos et facilis id
+                            labore necessitatibus nemo, nesciunt nobis officiis, quibusdam quo quos rem repudiandae
+                            tenetur. Accusamus asperiores, culpa dolorem est et eum, exercitationem ipsa minima natus
+                            odit pariatur provident quidem reiciendis repudiandae saepe suscipit veniam vero. A amet
+                            deleniti esse est laudantium minima nisi placeat sunt temporibus vero! Dicta ducimus facere
+                            fugiat impedit in laborum ut voluptates? Aliquam at consequuntur culpa cum doloribus eveniet
+                            hic illum inventore, ipsa iure laboriosam laborum maxime modi neque odit pariatur quasi qui
+                            quod reiciendis, similique sit unde vel velit veniam voluptas voluptatem voluptatibus
+                            voluptatum! Ab aliquid aspernatur blanditiis consequatur cumque cupiditate, dolor dolorum
+                            ducimus ex hic illo, incidunt iste laborum minima molestias obcaecati officia officiis quasi
+                            quos rem repellat reprehenderit rerum saepe tempore ullam! Accusamus aliquam esse laboriosam
+                            provident rerum sit soluta vero? At commodi culpa cum deleniti doloremque doloribus earum
+                            enim est eum ex expedita impedit in iste labore laboriosam laudantium maxime, minus modi
+                            molestias mollitia natus necessitatibus nemo neque quis quisquam quod quos saepe tempore
+                            unde ut velit veritatis.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
 </body>
 </html>
