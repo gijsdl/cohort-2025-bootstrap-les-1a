a59bc68482a26980dc3714818a85dd8847d6bd56
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2026-04-07 11:43:57 +0200

    les 7

--- index.php
+++ index.php
@@ -8,41 +8,42 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
 <!-- les 3 -->
-<!--<nav class="navbar navbar-expand-sm bg-primary" data-bs-theme="dark">-->
-<!--    <div class="container-fluid">-->
-<!--        <a class="navbar-brand" href="index.php">Bedrijfsnaam</a>-->
-<!--        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--            <span class="navbar-toggler-icon"></span>-->
-<!--        </button>-->
-<!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-<!--            <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" aria-current="page" href="index.php">Home</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" href="#">Link</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item dropdown">-->
-<!--                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">-->
-<!--                        Dropdown-->
-<!--                    </a>-->
-<!--                    <ul class="dropdown-menu">-->
-<!--                        <li><a class="dropdown-item" href="#">Action</a></li>-->
-<!--                        <li><a class="dropdown-item" href="#">Another action</a></li>-->
-<!--                        <li><hr class="dropdown-divider"></li>-->
-<!--                        <li><a class="dropdown-item" href="#">Something else here</a></li>-->
-<!--                    </ul>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" aria-disabled="true">Disabled</a>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</nav>-->
+<nav class="navbar navbar-expand-sm bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Bedrijfsnaam</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <!-- les 1 en 2 -->
 <!--<div class="container-lg">-->
 <!--    <div class="row">-->
@@ -113,18 +114,125 @@
 <!--</div>-->
 
 <!-- Les 5 -->
-<div class="container-fluid">
-    <div class="row bg-warning">
-        <div class="col d-flex flex-column flex-lg-row justify-content-around align-items-center">
-            <a href="#">Home</a>
-            <a href="#">page 1</a>
-            <a href="#">page 2</a>
-            <img src="img/logo.png" class="img-fluid" alt="logo">
+<!--<div class="container-fluid">-->
+<!--    <div class="row bg-warning">-->
+<!--        <div class="col d-flex flex-column flex-lg-row justify-content-around align-items-center">-->
+<!--            <a href="#">Home</a>-->
+<!--            <a href="#">page 1</a>-->
+<!--            <a href="#">page 2</a>-->
+<!--            <img src="img/logo.png" class="img-fluid" alt="logo">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+
+<!-- les 6 -->
+<!--<div class="container">-->
+<!--    <form class="row g-3 needs-validation" novalidate>-->
+<!--        <div class="col-md-4">-->
+<!--            <label for="validationCustom01" class="form-label">First name</label>-->
+<!--            <input type="text" class="form-control" id="validationCustom01" required>-->
+<!--            <div class="valid-feedback">-->
+<!--                Looks good!-->
+<!--            </div>-->
+<!--            <div class="invalid-feedback">-->
+<!--                Je hebt het fout ingevuld.-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-4">-->
+<!--            <label for="validationCustom02" class="form-label">Last name</label>-->
+<!--            <input type="text" class="form-control" id="validationCustom02" required>-->
+<!--            <div class="valid-feedback">-->
+<!--                Looks good!-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-4">-->
+<!--            <label for="validationCustomUsername" class="form-label">Username</label>-->
+<!--            <div class="input-group has-validation">-->
+<!--                <span class="input-group-text" id="inputGroupPrepend">@</span>-->
+<!--                <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>-->
+<!--                <div class="invalid-feedback">-->
+<!--                    Please choose a username.-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-6">-->
+<!--            <label for="validationCustom03" class="form-label">City</label>-->
+<!--            <input type="text" class="form-control" id="validationCustom03" required>-->
+<!--            <div class="invalid-feedback">-->
+<!--                Please provide a valid city.-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-3">-->
+<!--            <label for="validationCustom04" class="form-label">State</label>-->
+<!--            <select class="form-select" id="validationCustom04" required>-->
+<!--                <option selected disabled value="">Choose...</option>-->
+<!--                <option>...</option>-->
+<!--            </select>-->
+<!--            <div class="invalid-feedback">-->
+<!--                Please select a valid state.-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-3">-->
+<!--            <label for="validationCustom05" class="form-label">Zip</label>-->
+<!--            <input type="text" class="form-control" id="validationCustom05" required>-->
+<!--            <div class="invalid-feedback">-->
+<!--                Please provide a valid zip.-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-12">-->
+<!--            <div class="form-check">-->
+<!--                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>-->
+<!--                <label class="form-check-label" for="invalidCheck">-->
+<!--                    Agree to terms and conditions-->
+<!--                </label>-->
+<!--                <div class="invalid-feedback">-->
+<!--                    You must agree before submitting.-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-12">-->
+<!--            <button class="btn btn-primary" type="submit">Submit form</button>-->
+<!--        </div>-->
+<!--        <div class="form-check form-switch">-->
+<!--            <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">-->
+<!--            <label class="form-check-label" for="switchCheckDefault">Default switch checkbox input</label>-->
+<!--        </div>-->
+<!--    </form>-->
+<!--</div>-->
+
+<!--Les 7 -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <ol class="list-group list-group-numbered">
+                <li class="list-group-item list-group-item-success">1</li>
+                <li class="list-group-item">2</li>
+                <li class="list-group-item">3</li>
+                <li class="list-group-item">4</li>
+                <li class="list-group-item">5</li>
+            </ol>
+        </div>
+        <div class="col">
+            <button class="btn btn-outline-primary btn-lg">Text</button>
+            <button class="btn btn-primary">Text</button>
+            <button class="btn btn-danger">Text</button>
         </div>
     </div>
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Holy guacamole!</strong> You should check in on some of those fields below.<i class="bi bi-linkedin text-success fs-3"></i>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
